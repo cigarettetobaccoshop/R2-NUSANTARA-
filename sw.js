9869107907c259418a7519e756e80621b3212b86
cigarettetobaccoshop <cigaratetobacoshop@gmail.com>
2026-08-11 11:43:16 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,16 @@
-const CACHE_NAME = 'r2-nusantara-v2';
+const CACHE_NAME = 'r2-nusantara-v3';
 const urlsToCache = [
   '/R2-Nusantara/',
   '/R2-Nusantara/index.html',
   '/R2-Nusantara/style.css',
   '/R2-Nusantara/app.js',
   '/R2-Nusantara/data.js',
-  '/R2-Nusantara/manifest.json',
-  'https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,300..700&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+  '/R2-Nusantara/manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).catch(function(){})
   );
 });
 
