fb6473dd7cfb756cba9183b02f0206845cb8a03a
cigarettetobaccoshop <cigaratetobacoshop@gmail.com>
2026-08-15 10:59:59 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'r2-nusantara-v4';
+const CACHE_NAME = 'r2-nusantara-v3';
 const urlsToCache = [
-  '/id.com/',
-  '/id.com/index.html',
-  '/id.com/style.css',
-  '/id.com/app.js',
-  '/id.com/data.js',
-  '/id.com/manifest.json'
+  '/R2-Nusantara/',
+  '/R2-Nusantara/index.html',
+  '/R2-Nusantara/style.css',
+  '/R2-Nusantara/app.js',
+  '/R2-Nusantara/data.js',
+  '/R2-Nusantara/manifest.json'
 ];
 
 self.addEventListener('install', event => {
